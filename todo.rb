21729a5d0205da39e8904a9293cbd3b3fe1e47e9
sk2237 <suman77327@gmail.com>
2021-02-11 11:31:40 +0530

    My first todo program

---
+++ todo.rb
@@ -0,0 +1,8 @@
+#puts and gets
+#string interpolation 
+#variables
+puts "Addition of two numbers (10,21) :#{10 + 21}"
+puts "What is your next todo?"
+next_todo=gets	#input from user
+puts "\n" #new line
+puts "[ ] #{next_todo} " #string interpolation
